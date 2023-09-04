88dd04a9a488651ae78dee58cbb6e1d51eafc2f4
Durgasai001 <durgasaichowdary967@gmail.com>
2023-09-04 11:43:25 +0530

    sai

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg' ;
+import './App.css';
+import Todolist from './todolist';
+function App() {
+    return (
+        <div className="hello">
+            <h1>sai</h1>
+            <Todolist></Todolist>
+        </div>
+        
+    );
+}
+
+export default App;
